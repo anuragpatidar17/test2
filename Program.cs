7e242fb17edbcd7552af4107ce6f51ac68ff71fd
anuragpatidar17 <anuragpatidar17@gmail.com>
2021-02-12 11:20:13 +0000

    inital commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var driver = new ChromeDriver();
+
+            
+
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+
+            Console.WriteLine(driver.Title + " Checking");
+
+                
+                query.Submit();
+
+
+
+                driver.Quit();
+            
+
+
+
+
+        }
+    }
+}
